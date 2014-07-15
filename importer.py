d637876b28fdb1c6e81e28a15b0990fde5005216
xtourena <xtourena@users.noreply.github.com>
2014-07-15 13:58:25 +0200

    importer.py

---
+++ importer.py
@@ -0,0 +1,18 @@
+from sys import argv
+from termcolor import colored
+
+script, filename = argv
+
+print "----------------------------------"
+print colored ("Welcom to text(.txt) importer.", "red")
+print "----------------------------------"
+
+txt = open(filename)
+
+print "--------------------------------------------"
+print "Here's text from your file %r:" % filename
+print colored (txt.read(),"green")
+print "-------------------------------------------"
+
+print "Thanks for using "
+print "--------------------------------------------------"
